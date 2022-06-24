64b7f1cd65a5701e30c7cc3c5b15490abeda0d51
mriduldas07 <mriduldas0325@gmail.com>
2022-06-24 17:51:29 +0600

    order section by user

---
+++ src/user/Order.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { toast } from 'react-toastify';
+
+const Order = ({ details, index, refetch }) => {
+    const {
+        customer_name,
+        customer_email,
+        customer_phone,
+        product_name,
+        quantity,
+        price,
+        shipping_address,
+        productId
+    } = details;
+
+    const handleOrderCancel = () => {
+        const proceed = window.confirm("Are you sure to delete this order?");
+        if (proceed) {
+            fetch(`http://localhost:5000/orderCancel/${productId}`, {
+                method: "DELETE",
+            })
+                .then(res => res.json())
+                .then(data => {
+                    refetch();
+                    toast.success("Order delete successfully done");
+                });
+        };
+    };
+    return (
+        <tr>
+            <th>{index + 1}</th>
+            <td>{customer_name}</td>
+            <td>{customer_email}</td>
+            <td>{customer_phone}</td>
+            <td>{product_name}</td>
+            <td>{quantity}</td>
+            <td>$ {price}</td>
+            <td>{shipping_address}</td>
+            <td>{productId}</td>
+            <td>
+                <button className='btn btn-error btn-outline btn-sm' onClick={handleOrderCancel}>
+                    Delete Order
+                </button>
+            </td>
+        </tr>
+    );
+};
+
+export default Order;
--- src/user/Orders.js
+++ src/user/Orders.js
@@ -1,9 +1,58 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useQuery } from 'react-query';
+import auth from '../firebase.init';
+import Loading from '../Shared/Loading';
+import Order from './Order';
 
 const Orders = () => {
+    const [user, loading, error] = useAuthState(auth);
+
+    const { isLoading, queryError, refetch, data } = useQuery('orderData', () =>
+        fetch(`http://localhost:5000/orders/${user?.email}`, {
+            method: "GET",
+        }).then(res => res.json())
+    );
+
+    if (loading || isLoading) {
+        return <Loading></Loading>
+    }
+
+    if (error || queryError) {
+        console.log(error?.message);
+    }
+
     return (
         <div>
-            <h2>My order</h2>
+            <h2 className="text-success text-2xl">Total Order : {data.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table table-compact w-full">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                            <th>Product</th>
+                            <th>Quantity</th>
+                            <th>Per Price</th>
+                            <th>Shipping Address</th>
+                            <th>Order ID</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((order, index) => (
+                            <Order
+                                key={order._id}
+                                index={index}
+                                details={order}
+                                refetch={refetch}
+                            />
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
