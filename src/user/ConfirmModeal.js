73081d3ee7ea220de6d18ef907bede1442e5df6b
mriduldas07 <mriduldas0325@gmail.com>
2022-07-20 04:38:15 +0600

    review table and delete review

---
+++ src/user/ConfirmModeal.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ConfirmModeal = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ConfirmModeal;
--- src/user/Review.js
+++ src/user/Review.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useQuery } from 'react-query';
+import { toast } from 'react-toastify';
+import auth from '../firebase.init';
+import Loading from '../Shared/Loading';
 import AddReviewModal from './AddReviewModal';
 
 const Review = () => {
+    const [user, loading, error] = useAuthState(auth);
+    const [data, setData] = useState([]);
+    const [isReload, setIsReload] = useState(false)
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/review/${user?.email}`)
+            .then(res => res.json())
+            .then(result => setData(result))
+    }, [data]);
+
+    const handleReviewCancel = id => {
+        const proceed = window.confirm("Are you want to delete this review??");
+        if (proceed) {
+            fetch(`http://localhost:5000/review/${id}`, {
+                method: "DELETE",
+            })
+                .then(res => res.json())
+                .then(result => {
+                    toast.warning(`Delete your review is that => ${data[0].userReview}`)
+                    setIsReload(!isReload)
+                })
+        }
+    }
+
+
+    if (loading) {
+        return <Loading></Loading>
+    }
     return (
         <div className="">
             <label className="btn btn-success btn-outline" htmlFor="AddReview-Modal">
@@ -21,7 +56,23 @@ const Review = () => {
                             </tr>
                         </thead>
                         <tbody>
-
+                            {
+                                data?.map((review, index) => {
+                                    return (
+                                        <tr key={index}>
+                                            <td>{index = index + 1}</td>
+                                            <td>{review.toolName}</td>
+                                            <td>{review.userReview}</td>
+                                            <td>{review.userRating}</td>
+                                            <td>
+                                                <button className='btn btn-error btn-outline btn-sm' onClick={() => handleReviewCancel(review._id)}>
+                                                    Delete Review
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    )
+                                })
+                            }
                         </tbody>
                     </table>
                 </div>
